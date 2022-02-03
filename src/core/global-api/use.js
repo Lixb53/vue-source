b53bcabbc7ca07a80d753a360fbca6445094bd2c
Lixb53 <44687628+Lixb53@users.noreply.github.com>
2022-02-03 21:16:19 +0800

    Update use.js

--- src/core/global-api/use.js
+++ src/core/global-api/use.js
@@ -10,18 +10,25 @@ import { toArray } from '../util/index'
 export function initUse (Vue: GlobalAPI) {
   Vue.use = function (plugin: Function | Object) {
     const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
+    // 如果已经存在, 则直接返回 this(Vue)
     if (installedPlugins.indexOf(plugin) > -1) {
       return this
     }
 
     // additional parameters
+    // 额外的参数
     const args = toArray(arguments, 1)
+    // 把 this(Vue) 作为数组的第一项
     args.unshift(this)
+    // 如果插件的 install 属性是函数, 调用它
     if (typeof plugin.install === 'function') {
       plugin.install.apply(plugin, args)
     } else if (typeof plugin === 'function') {
+      // 如果插件是函数, 则调用它
+      // apply(null) 严格模式下, plugin 插件函数的 this 是null
       plugin.apply(null, args)
     }
+    // 添加到已安装的插件
     installedPlugins.push(plugin)
     return this
   }
